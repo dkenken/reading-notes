a7d6f03fb9f1c9c49dbdab58e9324fb6863a5b6f
Kenderly Delinois <dkenken72@gmail.com>
2021-10-23 21:51:02 -0700

    Changes

--- app1.js
+++ app1.js
@@ -1,9 +1,9 @@
-let userNametoLowerCas = prompt("What is your name?");
+let userNane = prompt("What is your name?");
 
 let userAge = prompt("What is your age?");
 
-if (userNametoLowerCas() == "ken") {
-  document.write("<h2>Welcome Monsieur " + userName + "<h2>");
+if (userNametoLowerCase() == "ken") {
+  document.write("<h2>Welcome Monsieur " + userNametoLowerCase() + "<h2>");
 } else {
   document.write("<h2>Welcome " + userNametoLowerCase() + "</h2>");
 }
